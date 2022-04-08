e4423b4cb918de31d936887d8f8cb5f531e34b20
CoolD69 <parthasingharoy166@gmail.com>
2022-04-08 13:18:34 +0530

    create sticker page

--- pages/sticker.js
+++ pages/sticker.js
@@ -2,7 +2,43 @@ import React from 'react'
 
 const Sicker = () => {
   return (
-    <div>Sicker</div>
+    <div className='bg-slate-100'>
+
+    <section className="text-gray-400  body-font">
+      <div className="container  px-5 py-24 lg:mx-40 mx-auto">
+
+        <div className="flex  flex-wrap -m-4">
+
+          {/* No:1 */}
+          <div className=" bg-white lg:w-1/5 border-2 md:w-1/2 p-2 m-2  w-full">
+            <a className="block relative h-72 rounded overflow-hidden">
+              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://rukminim2.flixcart.com/image/416/416/k3j1z0w0/sticker/m/s/f/white-glossy-modern-art540-large-61-white-glossy-modern-art540-original-imafmjygyfethq8x.jpeg?q=70" />
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500  tracking-widest title-font mb-1">Stickers</h3>
+              <h2 className="text-black title-font text-lg font-medium">The Catalyzer</h2>
+              <p className="mt-1">₹ 500.00</p>
+            </div>
+          </div>
+
+          {/* No:2 */}
+          <div className="lg:w-1/5 border-2 bg-white md:w-1/2 p-2 m-2 w-full">
+            <a className="block relative h-72 rounded overflow-hidden">
+              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://rukminim2.flixcart.com/image/416/416/k3j1z0w0/sticker/m/s/f/white-glossy-modern-art540-large-61-white-glossy-modern-art540-original-imafmjygyfethq8x.jpeg?q=70" />
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500  tracking-widest title-font mb-1">Stickers</h3>
+              <h2 className="text-black title-font text-lg font-medium">The Catalyzer</h2>
+              <p className="mt-1">₹ 500.00</p>
+            </div>
+          </div>
+        {/* No:3 */}
+
+
+        </div>
+      </div>
+    </section>
+  </div>
   )
 }
 
