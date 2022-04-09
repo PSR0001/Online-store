2816e758ce5c436db21187201cf0850b2580f27a
CoolD69 <parthasingharoy166@gmail.com>
2022-04-09 09:20:20 +0530

    Tshart page updated

--- pages/tshart.js
+++ pages/tshart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Link from 'next/link'
 const Tsharts = () => {
   return (
     <div className='bg-slate-100'>
@@ -12,7 +12,7 @@ const Tsharts = () => {
             {/* No:1 */}
             <div className=" bg-white lg:w-1/5 border-2 md:w-1/2 p-2 m-2  w-full">
               <a className="block relative h-72 rounded overflow-hidden">
-                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://rukminim2.flixcart.com/image/880/1056/kfoapow0-0/t-shirt/u/n/1/m-6555072-hrx-by-hrithik-roshan-original-imafw2ky4ewmgevh.jpeg?q=50" />
+               <Link href='Product/howdy'><img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://rukminim2.flixcart.com/image/880/1056/kfoapow0-0/t-shirt/u/n/1/m-6555072-hrx-by-hrithik-roshan-original-imafw2ky4ewmgevh.jpeg?q=50" /></Link>
               </a>
               <div className="mt-4">
                 <h3 className="text-gray-500  tracking-widest title-font mb-1">T-shart</h3>
