d27484ef632421aca3a7c8379cb3cd9b7f155d9b
CoolD69 <parthasingharoy166@gmail.com>
2022-04-11 20:34:54 +0530

    cart updated

--- components/Cart.js
+++ components/Cart.js
@@ -108,7 +108,7 @@ const Cart = ({fuck,cart, addToCart, deleteFromCart, clearCart, subTotal}) => {
             <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
                 <div className="flex justify-between text-base font-medium text-gray-900">
                     <p>Subtotal</p>
-                    <p>$262.00</p>
+                    <p>₹. {subTotal}</p>
                 </div>
                 <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                 <div className="mt-6">
