c279f5c357d7ce95346992dc8465b7dacf4b4153
CoolD69 <parthasingharoy166@gmail.com>
2022-04-23 22:22:33 +0530

    signup password encrypted

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,11 +1,15 @@
 import User from '../../models/User'
 import connectDB from '../../middleware/mongoose'
+import CryptoJS from"crypto-js"
+
 
 const handler = async(req, res)=> {
     if (req.method === 'POST') {
         // Process a POST request
         // console.log(req.body)
-       let U = new User(req.body)
+        let {name , email}=req.body;
+
+       let U = new User({name:name,email:email,password: CryptoJS.AES.encrypt(req.body.password, 'hello Guys').toString()})
       await U.save()
         res.status(200).json({ success:"success" })
 
