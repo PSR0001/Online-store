8052e4da5527938a8e323281f59816b738ad7f66
CoolD69 <parthasingharoy166@gmail.com>
2022-04-23 20:53:14 +0530

    signup page updated

--- pages/signup.js
+++ pages/signup.js
@@ -4,6 +4,10 @@ import Link from 'next/link'
 import { useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+
+
+
 const Signup = () => {
 
   const [name, setName] = useState()
