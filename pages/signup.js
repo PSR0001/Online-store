d9232f34c9c4606d639c736a4e0cb77b63fcb9d6
CoolD69 <parthasingharoy166@gmail.com>
2022-04-23 19:53:19 +0530

    signup page added use state

--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,34 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
 const Signup = () => {
+
+const [name, setName] = useState()
+const [email, setEmail] = useState()
+const [password, setPassword] = useState()
+const [cpassword, setCpassword] = useState()
+
+const fillForm=(e)=>{
+  //for debugging 
+    // console.log("hey U");
+    // console.log(e.target.value);
+    setName(e.target.value)
+
+
+}
+const submitSingup = async (e)=>{
+  // console.log("hi guys");
+  e.preventDefault()
+
+  const formBody ={}
+  // fetch("")
+  setName("")
+  setEmail('')
+  setPassword('')
+  setCpassword('')
+}
+
   return (
     <div className='flex flex-col bg-slate-100'>
       <div className=" flex items-center  justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -18,27 +45,28 @@ const Signup = () => {
             
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
+            <input  type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div >
                 <label htmlFor="name" className="sr-only">
                   Full Name
                 </label>
                 <input
-                  id="email-address"
+                  id="name"
                   name="name"
                   type="text"
                   required
                   className=" mt-2 mb-2 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Full Name"
+                  onChange={fillForm}
                 />
               </div>
               <div className='mt-2 mb-2'>
-                <label htmlFor="email-address" className="sr-only">
+                <label htmlFor="email" className="sr-only">
                   Email address
                 </label>
                 <input
-                  id="email-address"
+                  id="email"
                   name="email"
                   type="email"
                   autoComplete="email"
@@ -62,12 +90,12 @@ const Signup = () => {
                 />
               </div>
               <div >
-                <label htmlFor="password" className="sr-only">
+                <label htmlFor="cpassword" className="sr-only">
                   Confirm Password
                 </label>
                 <input
-                  id="password"
-                  name="password"
+                  id="cpassword"
+                  name="cpassword"
                   type="password"
                   autoComplete="current-password"
                   required
@@ -94,7 +122,7 @@ const Signup = () => {
             </div>
 
             <div>
-              <button
+              <button onClick={submitSingup}
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
